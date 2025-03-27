c38127dbdbfe155e434f6e5162cf9c369892fc25
jaess105 <jaess105@uni-duesseldorf.de>
2025-03-27 23:49:59 +0100

    add to seconds method for Time

--- champion-gandyr-challenge/src/champion_gandyr_challenge/models/time.cljs
+++ champion-gandyr-challenge/src/champion_gandyr_challenge/models/time.cljs
@@ -1,7 +1,9 @@
 (ns champion-gandyr-challenge.models.time)
 
-
 (defrecord Time [hour minute seconds]
   Object
   (toString [_]  (str hour "h " minute "m " seconds "s")))
+
+(defn to-seconds [{:keys [hour minute seconds]}]
+  (+ (* hour 3600) (* minute 60) seconds))
 
