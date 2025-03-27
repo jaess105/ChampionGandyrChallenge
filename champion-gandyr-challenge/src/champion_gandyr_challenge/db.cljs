f6f37e2cc553183be7afe8f13bc2394f562f8a4e
jaess105 <jaess105@uni-duesseldorf.de>
2025-03-27 21:38:06 +0100

    add some test data for now. Gonna remove it later

--- champion-gandyr-challenge/src/champion_gandyr_challenge/db.cljs
+++ champion-gandyr-challenge/src/champion_gandyr_challenge/db.cljs
@@ -1,4 +1,22 @@
-(ns champion-gandyr-challenge.db)
+(ns champion-gandyr-challenge.db
+  (:require
+   [champion-gandyr-challenge.models.time :refer [Time]]))
+
+
+(def ^:private score-board
+  [{:name "Testy2" :time (Time. 5 5 20)}
+   {:name "Testy3" :time (Time. 10 5 20)}
+   {:name "Testy4" :time (Time. 50 5 20)}
+   {:name "Testy1" :time (Time. 0 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}
+   {:name "Testyn" :time (Time. 60 5 20)}])
 
 (def default-db
-  {:name "re-frame"})
+  {:name "re-frame"
+   :score-board score-board})
