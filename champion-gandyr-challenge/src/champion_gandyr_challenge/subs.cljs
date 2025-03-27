28082923b297b640dd23c6d2f8febdd2a94c6675
jaess105 <jaess105@uni-duesseldorf.de>
2025-03-27 21:37:26 +0100

    rename re-frame to rf and add scoreboard sub

--- champion-gandyr-challenge/src/champion_gandyr_challenge/subs.cljs
+++ champion-gandyr-challenge/src/champion_gandyr_challenge/subs.cljs
@@ -1,8 +1,13 @@
 (ns champion-gandyr-challenge.subs
   (:require
-   [re-frame.core :as re-frame]))
+   [re-frame.core :as rf]))
 
-(re-frame/reg-sub
+(rf/reg-sub
  ::name
  (fn [db]
    (:name db)))
+
+(rf/reg-sub
+ :score-board
+ (fn [db _]
+   (:score-board db)))
