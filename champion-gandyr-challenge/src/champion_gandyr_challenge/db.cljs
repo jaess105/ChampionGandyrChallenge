7ba51e73ed4b1cfc5a4c7e88611ef9f5b6cb7865
Ninabeltin <nina.esser115pc@gmail.com>
2025-07-15 23:59:17 +0200

    Update db.cljs

--- champion-gandyr-challenge/src/champion_gandyr_challenge/db.cljs
+++ champion-gandyr-challenge/src/champion_gandyr_challenge/db.cljs
@@ -7,7 +7,8 @@
   [{:name "Nibeltin" :time (Time. 1 18 13)}
    {:name "TheValveGuy" :time (Time. 0 44 55)}
    {:name "JD Vape" :time (Time. 0 54 33)}
-   {:name "Chicken Jockey" :time (Time. 0 28 4)}])
+   {:name "Chicken Jockey" :time (Time. 0 28 4)}
+   {:name "Denis" :time (Time. 0 37 24)}])
 
 (def default-db
   {:name "re-frame"
